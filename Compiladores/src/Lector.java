4a69c5f282a104dbac32462c03ffe47dc6e53734
wrtfix <wrtfix@7ba92aa1-980a-f62f-b546-d50d34db8ab7>
2012-08-20 15:19:19 +0000

    Proyecto base para la produccion de un compilador

---
+++ Compiladores/src/Lector.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Esta clase me permitira realizar la lectura de un archivo de texto
+ * linea por lina.
+ * @author wrtfix
+ */
+public class Lector {
+    
+}
