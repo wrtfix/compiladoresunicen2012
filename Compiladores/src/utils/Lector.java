6910e3a5afcb3c0fb9c6aaac8f67fa3ae4cb3d42
mauripiccolo <mauripiccolo@7ba92aa1-980a-f62f-b546-d50d34db8ab7>
2012-08-20 18:51:56 +0000

    Issue 1: Se creó el método leerArchivo en la clase Lector para leer el contenido de un archivo txt. @mauripiccolo

--- Compiladores/src/utils/Lector.java
+++ Compiladores/src/utils/Lector.java
@@ -1,5 +1,11 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,5 +17,24 @@ package utils;
  * @author wrtfix
  */
 public class Lector {
-    
+ /**
+ * * Método que lee las líneas de un archivo txt una por una y las guarda en un arrayList de Strings.
+ * @param  ruta Contiene la ruta en donde se encuentra el archivo txt.
+ * @return Devuelve un arrayList que contine cada una de las líneas del archivo.
+ * @author mauripiccolo
+ */
+     public ArrayList<String> leerArchivo(String ruta){        
+        ArrayList<String> lineas = new ArrayList();
+        try {
+            FileReader fr = new FileReader(ruta);
+            BufferedReader bf = new BufferedReader(fr); 
+            String sCadena = "";
+            //Agrega la línea leída en el arreglo lineas
+            while ((sCadena = bf.readLine())!= null) {
+                lineas.add(sCadena);                
+            } 
+	} catch (FileNotFoundException fnfe){fnfe.printStackTrace();} 
+          catch (IOException ioe){ioe.printStackTrace();}			
+        return lineas;
+    }
 }
