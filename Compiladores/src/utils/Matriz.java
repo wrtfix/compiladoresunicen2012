ba0e40e097b053b4b0cfe221eeeb82e24fafa669
mauripiccolo <mauripiccolo@7ba92aa1-980a-f62f-b546-d50d34db8ab7>
2012-08-28 03:41:59 +0000

    Se creo el método getCelda que devuelve la celda segun el estado actual y el caracter leído. @mauripiccolo @jorgemendiola

--- Compiladores/src/utils/Matriz.java
+++ Compiladores/src/utils/Matriz.java
@@ -4,6 +4,7 @@
  */
 package utils;
 
+import java.util.Hashtable;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,7 +19,8 @@ public class Matriz {
     private Object celdas[][];
     private int fila;
     private int columna;
-
+    private Hashtable<Object,Integer> tabla; // elemento al que tenes que ir estado
+    
     /**
      * Para la creacion de una matriz se necesitan los siguientes parametros
      *
@@ -28,10 +30,48 @@ public class Matriz {
     public Matriz(int fila, int columna) {
         this.fila = fila;
         this.columna = columna;
-        this.celdas = new Object[fila][columna];
-       
+        this.celdas = new Object[fila][columna];        
     }
-
+    /**
+     * Este método devuelve la celda que corresponde al estado en que estamos actualmente y el caracter leído.
+     * @param caracter
+     * @param eActual
+     * @return estados y o acciones samanticas
+     */
+    public Object getCelda(char caracter, int eActual){                
+        Object celda = null;
+        if (Character.isLetter(caracter)&& eActual == 1 )
+            return get(eActual,1);//Es una letra
+        else if (Character.isDigit(caracter))
+                return get(eActual,2);//Es un digito
+        else 
+                    switch(caracter){
+                        case '+' :  celda = get(eActual,17); break;
+                        case '-' :  celda = get(eActual,17); break;
+                        case '*' :  celda = get(eActual,13); break;
+                        case '/' :  celda = get(eActual,6); break;
+                        case ':' :  celda = get(eActual,3); break;
+                        case '=' :  celda = get(eActual,4); break;
+                        case '>' :  celda = get(eActual,8); break;
+                        case '<' :  celda = get(eActual,7); break;
+                        case '.' :  celda = get(eActual,15); break;
+                        case '{' :  celda = get(eActual,5); break;
+                        case '}' :  celda = get(eActual,5); break;
+                        case '[' :  celda = get(eActual,5); break;
+                        case ']' :  celda = get(eActual,5); break;
+                        case '(' :  celda = get(eActual,5); break;
+                        case ')' :  celda = get(eActual,5); break;
+                        case ',' :  celda = get(eActual,5); break;
+                        case ';' :  celda = get(eActual,5); break;
+                        case '\t' : celda = get(eActual,10);break;
+                        case '\n' : celda = get(eActual,8); break;
+                        case '"' :  celda = get(eActual,14); break;
+                        case 'E' :  celda = get(eActual,16); break;                        
+                }
+        if(celda == null)
+            celda = get(eActual, 0);
+        return celda;
+    }
     /**
      * Para la creacion de matriz con elemento seteados se necesitan los siguientes parametros
      *
@@ -64,7 +104,7 @@ public class Matriz {
             Logger.getLogger(getClass().getName()).log(Level.WARNING, "La posicion "+fila+":"+columna+" se encuentra fuera de rango ");
         }
     }
-
+    
     /**
      * Este metodo verifica que no te vayas de rango y obtiene un elemento de la
      * matriz y requiere los siguientes parametros
