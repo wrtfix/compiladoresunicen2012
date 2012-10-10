35cb291e3b28f9edde2199c77c5976b4f677ebd5
mauripiccolo <mauripiccolo@7ba92aa1-980a-f62f-b546-d50d34db8ab7>
2012-10-10 03:41:47 +0000

    generador de codigo @mauripiccolo @damianVelazquezCafaro @jorgeMendiola

--- Compiladores/src/utils/GeneradorCodigo.java
+++ Compiladores/src/utils/GeneradorCodigo.java
@@ -4,6 +4,7 @@
  */
 package utils;
 
+import java.util.Hashtable;
 import java.util.Stack;
 
 /**
@@ -11,68 +12,80 @@ import java.util.Stack;
  * @author wrtfix
  */
 public class GeneradorCodigo {
-    private Stack<String> codigo;
+    private Stack<String> pilaCodigo;
     private Logger log;
     private TablaSimbolo ts;
+    private Hashtable<String,String> operadores = new Hashtable();;
+    
     public GeneradorCodigo(TablaSimbolo tabla){
-        codigo = new Stack<String>();
+        pilaCodigo = new Stack<String>();
         log = new Logger("logcodigo.log");
-        ts = tabla;
+        ts = tabla;        
+        operadores.put("Suma", "+");
+        operadores.put("Resta", "-");
+        operadores.put("Multiplicacion", "*");
+        operadores.put("Division", "/");
+        operadores.put("Asignacion", "/");
+        operadores.put("SaltoIcondicional", "/");
     }
     
     public void imprimirCodigo(){
-        for (int i=0; i<codigo.size();i++){
-            log.addLogger(codigo.get(i));
+        for (int i=0; i<pilaCodigo.size();i++){
+            log.addLogger(pilaCodigo.get(i));
         }
     }
     
-    public void ejecutarSuma(String der, String izq){
+    public float ejecutarSuma(String der, String izq){
         /*
          *  MOV AX, der
          *  ADD AX, izq
          *  MOV aux, AX
          */
+        return (Float.valueOf(der) + Float.valueOf(izq));
     }
-    public void ejecutarResta(String der, String izq){
+    public float ejecutarResta(String der, String izq){
         /*
          *  MOV AX, der
          *  SUB AX, izq
          *  MOV aux, AX
          */
-        
+        return(Float.valueOf(der) - Float.valueOf(izq));
     }
     
-    public void ejecutarMutiplicar(String der, String izq){
+    public float ejecutarMutiplicar(String der, String izq){
         /*
          *  MOV AX, der
          *  IMUL izq 
          *  MOV aux, AX
          */
+        return (Float.valueOf(der) * Float.valueOf(izq));
     }
     
-    public void ejecutarDividir(String der, String izq){
+    public float ejecutarDividir(String der, String izq){
         /*
          *  MOV AX, der
          *  DIV DX, 0
          *  IDIV izq
          *  MOV aux, AX
          */
+        return (Float.valueOf(der) / Float.valueOf(izq));
     }
     
-    public void ejecutarAsignacion(String der, String izq){
-        /*
-         * Buscar en la tabla de simbolos y se carga en el regitro EAX
-         */
+    public float ejecutarAsignacion(String der, String izq){
+        /* Mov Ax izq
+         * Mov dir der, Ax         
+         */             
+        return (Float.valueOf(der));
     }
     
     
-    public void ejecutarCondicion(String der, String izq){
+    public void ejecutarSaltoFalso(String salto){
         /*
          *  Los saltos deben ser cargados en el registro EDX
          */
     }
     
-    public void ejecutarIncondicional(String der, String izq){
+    public void ejecutarSaltoIncondicional(String salto){
         /*
          *  Preguntar
          */
@@ -80,10 +93,56 @@ public class GeneradorCodigo {
     
     
     
-    public void recorrerPolaca(Stack<String> polaca){
-        
+    public void recorrerPolaca(Stack<String> polaca){        
         for (int i=0; i<polaca.size();i++){
-            
+            float result = 0;
+            String varAux = polaca.get(i);            
+            if ( esOperador(varAux)){
+                // sumo desapilando el ultimo y ante ultimo y el resultado se apila
+                if("+".equals(varAux)){
+                    String id1 = pilaCodigo.pop();   
+                    String id2 = pilaCodigo.pop();                   
+                    result = ejecutarSuma(id1,id2);
+                }
+                //resta desapilando el ultimo y ante ultimo y el resultado se apila
+                if("-".equals(varAux)){
+                    String id1 = pilaCodigo.pop();   
+                    String id2 = pilaCodigo.pop();
+                    result = ejecutarResta(id1,id2);
+                }
+                //multiplicar desapilando el ultimo y ante ultimo y el resultado se apila
+                if("*".equals(varAux)){
+                    String id1 = pilaCodigo.pop();   
+                    String id2 = pilaCodigo.pop();
+                    result = ejecutarMutiplicar(id1,id2);
+                }
+                //dividir desapilando el ultimo y ante ultimo y el resultado se apila
+                if("/".equals(varAux)){
+                    String id1 = pilaCodigo.pop();   
+                    String id2 = pilaCodigo.pop();
+                    result = ejecutarDividir(id1,id2);
+                }                
+                if (":=".equals(varAux)){
+                    String id1 = pilaCodigo.pop();   
+                    String id2 = pilaCodigo.pop();
+                    result = ejecutarAsignacion(id1,id2);
+                }
+                if ("BI".equals(varAux)){
+                    String salto = pilaCodigo.pop();
+                    ejecutarSaltoIncondicional(salto);
+                }
+                if ("BF".equals(varAux)){
+                    String salto = pilaCodigo.pop();                
+                    ejecutarSaltoFalso(salto);
+                }
+                //apilar el resultado en la pila                
+                pilaCodigo.push(String.valueOf(String.valueOf(result)));
+            }
+            else
+               pilaCodigo.push(varAux);//apilo;                            
         }
+    }
+    public boolean esOperador(String varAux){
+        return (operadores.contains(varAux));
     }
 }
