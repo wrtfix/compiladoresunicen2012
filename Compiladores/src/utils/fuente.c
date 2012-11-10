bb60f59c349812998ceac1afb53225c7d68c0619
wrtfix@gmail.com <wrtfix@gmail.com@7ba92aa1-980a-f62f-b546-d50d34db8ab7>
2012-11-10 18:54:52 +0000

    Asi como esta

---
+++ Compiladores/src/utils/fuente.c
@@ -0,0 +1,25 @@
+array float e[6.];
+array float a[6.];
+array float b[6.];
+
+e[2.]:=4.;
+a[2.]:=12.;
+
+e[2.]:=e[2.]+e[2.];
+print('primera suma');
+b[1.]:=1.;
+e[2.]:=e[2.]+e[2.];
+print('segunda suma');
+
+
+
+if (e[2.]<a[2.])then
+{
+	print ('anda');
+
+}
+
+if (e[2.]<a[2.])then
+{
+	print ('anda2');	
+}
